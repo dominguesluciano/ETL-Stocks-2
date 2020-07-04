8fe1f0bbf168e16fc57653e68530aef4e50f71b2
ldp <dominguesluciano@gmail.com>
2020-07-04 12:29:12 -0400

    Update plot.js

--- web/static/js/plot.js
+++ web/static/js/plot.js
@@ -1,4 +1,4 @@
-let url = '../../templates/apiv1/apiv1.json';
+let url = 'https://raw.githubusercontent.com/dominguesluciano/ETL-Stocks-2/master/web/templates/apiv1/apiv1.json';
 var apiData =  d3.json(url)
 
 let button = d3.select("#filter-btn");
